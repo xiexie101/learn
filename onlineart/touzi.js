fed16ea6471fcbdea9e14687165e7b3d10ca8506
xiexie <sjaigg@gmail.com>
2025-03-19 17:51:08 +0800

    投资

---
+++ onlineart/touzi.js
@@ -0,0 +1,54 @@
+[
+    {
+        "code": "000991",
+        "name": "全指医药",
+        "current": 8584,
+        "support1": 9800,
+        "support2": 0,
+        "pressure1": 17300,
+        "pressure2": 20000,
+        "highest": 16906,
+        "highestDate": "2021-02-18",
+        "lowest": 7038,
+        "lowestDate": "2024-09-24"
+    },
+    {
+        "code": "399989",
+        "name": "中证医疗",
+        "current": 7134,
+        "support1": 8200,
+        "support2": 0,
+        "pressure1": 13000,
+        "pressure2": 15000,
+        "highest": 19992,
+        "highestDate": "2021-07-01",
+        "lowest": 5332,
+        "lowestDate": "2024-09-18"
+    },
+    {
+        "code": "399396",
+        "name": "食品饮料",
+        "current": 20031,
+        "support1": 17000,
+        "support2": 12210,
+        "pressure1": 40700,
+        "pressure2": 41700,
+        "highest": 40700,
+        "highestDate": "2021-02-18",
+        "lowest": 15353,
+        "lowestDate": "2024-09-18"
+    },
+    {
+        "code": "HSHCI",
+        "name": "恒生医疗",
+        "current": 2950,
+        "support1": 2900,
+        "support2": 2340,
+        "pressure1": 8396,
+        "pressure2": 8500,
+        "highest": 8396,
+        "highestDate": "2021-06-29",
+        "lowest": 1998,
+        "lowestDate": "2024-07-09"
+    }
+]
